3e036036315a59156e1d08eae29cec3fc3fe0b2e
byeongmin-kwak <bqudmals@naver.com>
2024-01-17 21:16:03 +0900

    init

---
+++ src/test/java/goorm/homeless/HomelessApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.homeless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
